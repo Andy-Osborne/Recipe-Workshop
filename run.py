5a7b64a013014abe81448780d3f35e31d64b62f5
Andy-Osborne <ajaosborne@googlemail.com>
2020-06-08 18:07:02 +0100

    Add mongoDB connections

--- run.py
+++ run.py
@@ -1,12 +1,29 @@
 import os
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect, request
 from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+from os import path
+if path.exists("env.py"):
+    import env
+
 
 app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ.get("NONGO_DBNAME")
+app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
+app.secret_key = os.environ.get("SECRET_KEY")
+
+mongo = PyMongo(app)
 
 @app.route('/')
 def index():
     return render_template("public/index.html")
+
+@app.route('/recipe')
+def get_recipe():
+
+    return render_template("public/recipe.html", recipes=mongo.db.recipe.find())
 
 
 if __name__ == '__main__':
