8bf0811b34dc039e150fdf884c9c1418a5f9b746
Andy-Osborne <ajaosborne@googlemail.com>
2020-06-12 09:47:57 +0100

    Add user interactivity to add or remove additional ingredient inputs to form

---
+++ static/js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+    
+    // The below adds an extra line for a recipe ingredient to the new_recipe.html
+    // when the corresponding button is clicked.
+    
+    $("#add_ingredient").on("click", () => {
+        $("#ingredient").append('<input class="form-control" type="text" name="ingredients">');
+    });
+
+    // The below removes the last recipe ingredient input when the corresponding button is clicked.
+
+    $("#remove_ingredient").on("click", () => {
+     $("#ingredient input:last-child").remove();   
+    });
+});
