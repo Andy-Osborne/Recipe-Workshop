634b416a6bdf97d42f4e00c1e7d7a8e54dba19e3
Andy-Osborne <ajaosborne@googlemail.com>
2020-06-23 15:22:20 +0100

    Add zoom effect to nav buttons

--- static/js/main.js
+++ static/js/main.js
@@ -1,23 +1,40 @@
 $(document).ready(function(){
     
-    // The below adds an extra line for a recipe ingredient to the new_recipe.html
-    // when the corresponding button is clicked.
+    /*
+    The below adds an extra line for recipe ingredient/step form in the new_recipe.html
+    when the corresponding button is clicked.
+    */
     
     $("#add_ingredient").on("click", () => {
         $("#ingredient").append('<input class="form-control" type="text" name="ingredients">');
     });
 
-    // The below removes the last recipe ingredient input when the corresponding button is clicked.
+    $("#add_step").on("click", () => {
+        $("#step_inputs").append('<input class="form-control" type="text" name="step">');
+    });
+    
+    /* 
+    The below removes a line for recipe ingredient/step form in the new_recipe.html
+    when the corresponding button is clicked.
+    */
 
     $("#remove_ingredient").on("click", () => {
      $("#ingredient input:last-child").remove();   
     });
 
-    $("#add_step").on("click", () => {
-        $("#step_inputs").append('<input class="form-control" type="text" name="step">');
-    });
-
     $("#remove_step").on("click", () => {
         $("#step_inputs input:last-child").remove();
     });
+
+    // The below applies/removes the zoom class on the nav buttons when the mouse enters/leaves
+
+    $(".btn-sign-up").on("mouseenter", event => {
+        $(event.currentTarget).addClass("btn-zoom");
+    });
+
+    $(".btn-sign-up").on("mouseleave", event => {
+        $(event.currentTarget).removeClass("btn-zoom");
+    });
+
+    
 });
