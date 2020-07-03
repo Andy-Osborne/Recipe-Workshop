e599789558625a357f0fbbfc4b2c272954299850
Andy-Osborne <ajaosborne@googlemail.com>
2020-07-03 13:23:53 +0100

    Fix spelling for cloudinary url

--- run.py
+++ run.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
-app.config["CLOUDINARY_URL"] = os.environ.get("CLOUDINARY_UR")
+app.config["CLOUDINARY_URL"] = os.environ.get("CLOUDINARY_URL")
 
 mongo = PyMongo(app)
 
