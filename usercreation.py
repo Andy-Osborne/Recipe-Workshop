27e62d526ad29c2fb309614f60b0542410abad61
Andy-Osborne <ajaosborne@googlemail.com>
2020-07-26 16:59:28 +0100

    Import regex, add regex requirement to user creation. Implement min password length

--- usercreation.py
+++ usercreation.py
@@ -1,13 +1,18 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, Regexp
 
 
 class UserRegistration(FlaskForm):
-    username = StringField("Username", validators=[DataRequired(),
-                                                   Length(min=3, max=20)])
+    username = StringField("Username",
+                           validators=[DataRequired(), Length(min=3, max=20),
+                                       Regexp('^[a-zA-Z0-9]*$',
+                                              message="Please use only \
+                                                   lowercase, uppercase or \
+                                                        numbers")])
     email = StringField("Email", validators=[DataRequired(), Email()])
-    password = PasswordField("Password", validators=[DataRequired()])
+    password = PasswordField("Password", validators=[DataRequired(),
+                                                     Length(min=8)])
     confirm_password = PasswordField("Confirm Password",
                                      validators=[DataRequired(),
                                                  EqualTo("password")])
